a98f4e2a249daa44cc40367b551ae91fba46eba8
acicalo2 <70492912+acicalo2@users.noreply.github.com>
2023-04-05 12:56:48 -0400

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Building the container requires the manual download of cellranger in the build directory
 # From: https://support.10xgenomics.com/single-cell-gene-expression/software/downloads/latest
-docker build --no-cache . -t bwh_bioinformatics_pipelines/nextflow-scRNAseq:latest
+sudo docker build .
